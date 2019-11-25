0be65d5b003719c2afe47f17e61d4818da4a31ae
Tatiana Panferova <tatiana.panferova@gmail.com>
2019-11-25 21:53:03 +0100

    Added styling and finished functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React, { useState } from 'react';
 import './App.css';
 
-const Todo = ({ todo, index, completeTask }) => {
+const Todo = ({ todo, index, completeTask, deleteTask }) => {
   return (
-    <>
-      <li onClick={() => completeTask(index)} 
-      style={{ textDecoration: todo.isCompleted ? 'line-through' : '' }}>
+    <div className='todo-item'>
+      <li onClick={() => completeTask(index)}
+        style={{ textDecoration: todo.isCompleted ? 'line-through' : '' }}>
         {todo.text}</li>
-      <button>Delete</button>
-    </>
+      <button id='delete' onClick={() => deleteTask(index)}>DELETE</button>
+    </div>
   )
 }
 
 function App() {
 
   const [todos, setTodos] = useState([
-    { text: 'Learn React Hooks', isCompleted: false }, 
-    { text: 'Master JavaScript', isCompleted: false }, 
-    {text: 'Learn Ruby', isCompleted: false}
+    { text: 'Learn React Hooks', isCompleted: false },
+    { text: 'Master JavaScript', isCompleted: false },
+    { text: 'Learn Ruby', isCompleted: false }
   ]);
 
   const [value, setValue] = useState('');
@@ -27,7 +27,6 @@ function App() {
     if (!value) {
       return;
     }
-    console.log(value)
     addTodo(value);
     setValue('');
   }
@@ -37,28 +36,46 @@ function App() {
   }
 
   const completeTask = index => {
-     const newTodos = [...todos];
-     newTodos[index].isCompleted = !newTodos[index].isCompleted;
-     setTodos(newTodos);
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = !newTodos[index].isCompleted;
+    setTodos(newTodos);
+  }
+
+  const deleteTask = index => {
+    const todoList = [...todos];
+    todoList.splice(index, 1);
+    setTodos(todoList);
   }
 
   return (
     <div className="App">
-      <h1>TODO List</h1>
-      <ul>{todos.map((todo, index) => {
-        return <Todo
-          key={index}
-          index={index}
-          todo={todo}
-          completeTask={completeTask} />
-      })}
-      </ul>
-      <form onSubmit={handleSubmit}>
-        <input
-          type='text'
-          onChange={e => setValue(e.target.value)}
-          value={value} />
-      </form>
+      <div className='wrapper'>
+      <div>
+        <h1>TODO List</h1>
+      </div>
+      <div className='todo-list'>
+        <ul>{todos.map((todo, index) => {
+          return <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            completeTask={completeTask}
+            deleteTask={deleteTask} />
+        })}
+        </ul>
+      </div>
+      <div className='add-todo-section'>
+        <form className='form-group' onSubmit={handleSubmit}>
+          <input
+            id='todo-input'
+            type='text'
+            placeholder='Add new task...'
+            onChange={e => setValue(e.target.value)}
+            value={value} />
+          <button onClick={handleSubmit}>ADD</button>
+        </form>
+      </div>
+      </div>
     </div>
   );
 }
